1ed59f20e007bdd881ff1d9a40e3c492cfec8d8a
MartinLunaRogel <mluna50@alumnos.uaq.mx>
2024-10-31 21:11:51 -0600

    Modal para editar location

--- app/dashboard/@locations/_components/UpdateLocation.tsx
+++ app/dashboard/@locations/_components/UpdateLocation.tsx
@@ -1,19 +1,19 @@
 'use client';
-import {Modal, ModalContent, ModalBody, Button, useDisclosure} from "@nextui-org/react";
+import {Modal, ModalContent, ModalHeader, ModalBody, ModalFooter, Button, useDisclosure} from "@nextui-org/react";
 import { ReactNode } from "react";
-import { LuPencil } from "react-icons/lu";
+import { LuPenTool } from "react-icons/lu";
 
-export default function UpdateLocation({children, store}: {children: ReactNode, store: string | string[] | undefined}) {
-  if (!store) return <div />;
+export default function UpdateLocation({children}: {children: ReactNode}) {
   const {isOpen, onOpen, onOpenChange} = useDisclosure();
 
   return (
-    <div>
-      <Button onPress={onOpen} color="primary"><LuPencil size="20"/></Button>
-      <Modal className="bg-orange-400" isOpen={isOpen} onOpenChange={onOpenChange}>
-        <ModalContent className="w-full">
+    <>
+      <Button onPress={onOpen} color="primary"><LuPenTool size="20"/></Button>
+      <Modal isOpen={isOpen} onOpenChange={onOpenChange}>
+        <ModalContent>
           {() => (
             <>
+              <ModalHeader className="flex flex-col gap-1">Modal Title</ModalHeader>
               <ModalBody>
                 {children}
               </ModalBody>
@@ -21,6 +21,6 @@ export default function UpdateLocation({children, store}: {children: ReactNode, 
           )}
         </ModalContent>
       </Modal>
-    </div>
+    </>
   );
 }
