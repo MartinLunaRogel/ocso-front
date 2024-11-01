f93cc163ce317858189bac70ae087c47d19b75da
MartinLunaRogel <mluna50@alumnos.uaq.mx>
2024-10-31 21:17:31 -0600

    Manager card and content

--- app/dashboard/managers/[id]/page.tsx
+++ app/dashboard/managers/[id]/page.tsx
@@ -2,9 +2,6 @@ import { API_URL } from "@/constants";
 import { Manager } from "@/entities";
 import { authHeaders } from "@/helpers/authHeaders";
 import { Card, CardBody, CardHeader, Divider } from "@nextui-org/react";
-import ManagerCard from "./_components/ManagerCard";
-import DeleteManagerButton from "./_components/DeleteManagerButton";
-import { Suspense } from "react";
 export default async function ManagerPage({
   params,
 }: {
@@ -17,16 +14,42 @@ export default async function ManagerPage({
       ...authHeaders(),
     },
     next: {
-      tags: [`dashboard:managers:${params.id}`, `dashboard:managers`],
-    },
+      tags: [`dashboard:managers:${params.id}`, `dashboard:managers`]
+    }
   });
   const data: Manager = await response.json();
   return (
-    <div className="flex flex-col gap-10 flex-grow-0 items-center justify-center">
-      <ManagerCard manager={data} />
-      <div className="bg-white shadow-medium rounded-md px-10 py-2">
-        <DeleteManagerButton managerId={data.managerId} />
-      </div>
-    </div>
+      <Card className="mx-20 py-2 bg-orange-50">
+        <CardHeader>
+          <p className="w-full">
+            Nombre: <b>{data.managerFullName}</b>
+          </p>
+        </CardHeader>
+        <Divider />
+        <CardBody>
+          <p className="w-full">
+            Email: <b>{data.managerEmail}</b>
+          </p>
+          <p className="w-full">
+            Teléfono: <b>{data.managerPhoneNumber}</b>
+          </p>
+          {data.location ? (
+            <>
+              <p>
+                Tienda: <b>{data.location.locationName}</b>
+              </p>
+              <iframe
+                className="border-2 border-orange-800 rounded-md my-2"
+                width="300"
+                height="200"
+                src={`https://www.google.com/maps/embed/v1/place?key=AIzaSyAz0Y6dhhUVleZmt7-H4PO1QQWCSEz3LBg
+                    &q=${data.location.locationLatLng[0]},${data.location.locationLatLng[1]}`}
+              ></iframe>
+            </>
+          ) : (
+            <p> No tiene tienda </p>
+          )}
+        </CardBody>
+      </Card>
   );
 }
--- app/dashboard/managers/layout.tsx
+++ app/dashboard/managers/layout.tsx
@@ -1,20 +1,12 @@
 import { ReactNode } from "react";
 import ManagerCards from "./_components/ManagerCards";
-export default function LayoutManagers({
-  children,
-  count,
-}: {
-  children: ReactNode;
-  count: ReactNode;
-}) {
+export default function LayoutManagers({ children }: { children: ReactNode }) {
   return (
     <>
       <div className="w-4/12 max-h-[90vh] h-[90vh] overflow-hidden overflow-y-auto">
         <ManagerCards />
       </div>
-      <div className="w-7/12 flex flex-col justify-center items-center gap-10">
-        <div>{children}</div> <div> {count}</div>
-      </div>
+      <div>{children}</div>
     </>
   );
 }
