aaed7e603df290f491b4fd63a7b4c9c610e551b8
MartinLunaRogel <mluna50@alumnos.uaq.mx>
2024-10-31 20:34:09 -0600

    feat: Server actions for managers

---
+++ app/dashboard/managers/[id]/page.tsx
@@ -0,0 +1,27 @@
+import { API_URL } from "@/constants";
+import { Manager } from "@/entities";
+import { authHeaders } from "@/helpers/authHeaders";
+import { Card, CardBody, CardHeader, Divider } from "@nextui-org/react";
+import ManagerCard from "./_components/ManagerCard";
+export default async function ManagerPage({
+  params,
+}: {
+  params: {
+    id: string;
+  };
+}) {
+  const response = await fetch(`${API_URL}/managers/${params.id}`, {
+    headers: {
+      ...authHeaders(),
+    },
+    next: {
+      tags: [`dashboard:managers:${params.id}`, `dashboard:managers`],
+    },
+  });
+  const data: Manager = await response.json();
+  return (
+    <div >
+      <ManagerCard manager={data} />
+    </div>
+  );
+}
